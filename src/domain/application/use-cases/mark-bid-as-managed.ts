12b8544f9d6198c2ff7e533d73142cb3657310d2
guicrosariol <guilhermecrosariol@gmail.com>
2025-05-25 08:16:14 -0300

    refactor(use-case): improve mark-bid-as-managed logic

--- src/domain/application/use-cases/mark-bid-as-managed.ts
+++ src/domain/application/use-cases/mark-bid-as-managed.ts
@@ -1,25 +1,40 @@
 import { left, right, type Either } from "../../../core/either";
 import type { BidRepository } from "../repositories/bid-repository";
+import type { CompanyRepository } from "../repositories/company-repository";
 import { ActionAlreadyPerformedError } from "./errors/action-already-performed-error";
+import { ForbiddenError } from "./errors/forbidden-error";
 import { NotFoundError } from "./errors/not-found-error";
 
 interface MarkBidAsManagedRequest {
   bidId: string
+  companyId: string
 }
 
 type MarkBidAsManagedResponse = Either<ActionAlreadyPerformedError | NotFoundError, null>
 
 export class MarkBidAsManagedUseCase {
   constructor(
+    private companyRepository: CompanyRepository,
     private bidRepository: BidRepository
   ) { }
   async execute({
+    companyId,
     bidId
   }: MarkBidAsManagedRequest): Promise<MarkBidAsManagedResponse> {
-    const doesBidExist = await this.bidRepository.findById(bidId)
+    const bid = await this.bidRepository.findById(bidId)
 
-    if (!doesBidExist) {
+    if (!bid) {
       return left(new NotFoundError())
+    }
+
+    const doesCompanyExist = await this.companyRepository.findById(companyId)
+
+    if (!doesCompanyExist) {
+      return left(new NotFoundError())
+    }
+
+    if (bid.props.companyId != companyId) {
+      return left(new ForbiddenError())
     }
 
     const isManaged = await this.bidRepository.checkIfManaged(bidId)
