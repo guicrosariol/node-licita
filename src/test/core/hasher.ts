a34e5ccf1c6a12556e2eea592598118b54f64c7b
guicrosariol <guilhermecrosariol@gmail.com>
2025-05-27 14:30:51 -0300

    chore: moved tests to project root

--- src/test/core/hasher.ts
+++
@@ -1,7 +0,0 @@
-import type { Hasher } from "../../core/hasher";
-
-export class FakeHasher implements Hasher {
-  async hash(plain: string, salt: number): Promise<string> {
-    return `${plain}-hashed-${salt}`
-  }
-}
