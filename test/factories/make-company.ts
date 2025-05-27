0e40b867c582c1deca2c42b3b2c2573f1a70c251
guicrosariol <guilhermecrosariol@gmail.com>
2025-05-27 14:31:57 -0300

    chore: moved tests to project root

---
+++ test/factories/make-company.ts
@@ -0,0 +1,15 @@
+import { Company, type CompanyProps } from "../../src/domain/entities/company";
+
+export function makeCompany(override: Partial<CompanyProps> = {}, id?: string) {
+  return Company.create({
+    name: 'example',
+    email: 'example@example.com',
+    cnpj: '1',
+    ownerId: '1',
+    currentUsers: 1,
+    maximumUsers: 1,
+    ...override,
+  }, id)
+}
+
+
---
+++ test/repositories/in-memory-company-repository.ts
@@ -0,0 +1,31 @@
+import type { CompanyRepository } from "../../src/domain/application/repositories/company-repository"
+import type { Company } from "../../src/domain/entities/company"
+
+export class InMemoryCompanyRepository implements CompanyRepository {
+  private companies: Company[] = []
+
+  async findById(companyId: string): Promise<null | Company> {
+    const company = this.companies.find((company) => company.id.toString() === companyId)
+
+    if (!company) {
+      return null
+    }
+
+    return company
+  }
+
+  async create(company: Company): Promise<Company> {
+    this.companies.push(company)
+    return company
+  }
+
+  async findByEmailOrCnpj(email: string, cnpj: string): Promise<Company | null> {
+    const company = this.companies.find((company) => company.props.email == email || company.props.cnpj == cnpj)
+
+    if (!company) {
+      return null
+    }
+
+    return company
+  }
+}
