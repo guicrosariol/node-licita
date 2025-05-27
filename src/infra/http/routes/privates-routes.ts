28d69247d225e8d5d9742be941fa1f9f73a0c30a
guicrosariol <guilhermecrosariol@gmail.com>
2025-05-27 14:34:27 -0300

    feat(route): add new private route

--- src/infra/http/routes/privates-routes.ts
+++ src/infra/http/routes/privates-routes.ts
@@ -1,5 +1,6 @@
 import type { FastifyInstance } from "fastify";
+import { createCompanyController } from "../controllers/create-company-controller";
 
 export function privateRoutes(app: FastifyInstance) {
-  
+  app.post('/company', { onRequest: [] }, createCompanyController)
 }
